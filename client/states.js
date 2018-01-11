9918cfb0e5e6b2298a045ef35c85490ac5a8134c
Christian Sehn <christian.sehn@flex.com>
2018-01-11 00:12:00 -0800

    [Studio] Add screen "tracker"

--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.tracker', {
+        views: {
+            app: {
+                controller: 'app_tracker',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.tracker');
+                }
+            }
+        }
     });
 });
